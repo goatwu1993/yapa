e203220c6c4e72e0808f2c522e5851dbc2e620d0
goatwu1993 <peteeelol@gmail.com>
2022-12-21 16:38:49 +0800

    test: add some test cases

--- tests/test_bool_assertions.py
+++ tests/test_bool_assertions.py
@@ -4,3 +4,7 @@ from yapc import expect
 def test_bool_ok():
     expect(True).to.be.ok
     expect(False).to.be._not.ok
+    expect(True).to.be.true
+    expect(False).to.be.false
+    expect(True)._not.to.be.false
+    expect(False)._not.to.be.true
