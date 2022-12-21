7c4dcafffaccf3c4f60c3f16ac06127f89d876df
goatwu1993 <peteeelol@gmail.com>
2022-12-21 16:40:53 +0800

    test: add some test cases

--- tests/test_bool_assertions.py
+++ tests/test_bool_assertions.py
@@ -8,3 +8,7 @@ def test_bool_ok():
     expect(False).to.be.false
     expect(True)._not.to.be.false
     expect(False)._not.to.be.true
+    expect(True).to.be.a(bool)
+    expect(False).to.be.a(bool)
+    expect(True)._not.to.be.a(str)
+    expect(False)._not.to.be.a(str)
