e53eee2475380d74320727b2675b7ad8470f7c0a
goatwu1993 <peteeelol@gmail.com>
2023-03-13 14:27:57 +0800

    chore: fix mypy error

--- yapa/impl/assertions/string_assertion.py
+++ yapa/impl/assertions/string_assertion.py
@@ -1,23 +1,27 @@
 from typing import Type
 
-from .generic_assertion import GenericAssertion
+from typing_extensions import Self
 
 from yapa.flags import StrFlags
+
+from .generic_assertion import GenericAssertion
 
 
 class StrAssertion(GenericAssertion):
     def __init__(self, actual: str, flag_class: Type[StrFlags]):
         return super().__init__(actual, flag_class)
 
-    def __call__(self, dest: str):
+    def __call__(self, dest: str) -> Self:
         if self.flags.have:
             assert (dest in self.actual) is self.flags.tobe
             return self
         assert (dest == self.actual) is self.flags.tobe
         return self
 
-    def string(self, dest: str) -> "StrAssertion":
-        return self(dest=dest)
+    def string(self, dest: str) -> Self:
+        self(dest=dest)
+        return self
 
-    def str(self, dest: str) -> "StrAssertion":
-        return self(dest=dest)
+    def str(self, dest: str) -> Self:
+        self(dest=dest)
+        return self
