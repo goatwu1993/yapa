40c1628a2b912be0dfe6a97c639083e16a090a6e
goatwu1993 <peteeelol@gmail.com>
2023-03-16 14:22:59 +0800

    test: refactor test

--- tests/test_str_assertions.py
+++ tests/test_str_assertions.py
@@ -3,9 +3,11 @@ from yapa import expect
 
 def test_assertions_str_positive():
     # tobe
-    expect("foobar").to_be("foobar")
-    expect("foobar").to_contain("bar")
-    expect("foobar").to_be_truthy()
-    expect("foobar").to_be_identical_to("foobar")
-    expect("foobar").to_be_lower_case()
-    expect("FOOBAR").to_be_upper_case()
+    s = "foobar"
+    expect(s).to_be(s)
+    expect(s).to_contain("bar")
+    expect(s).to_be_truthy()
+    expect(s).to_be_identical_to(s)
+    expect(s).to_be_lower_case()
+    expect(s.upper()).to_be_upper_case()
+    expect(s).to_be_identical_to(s)
